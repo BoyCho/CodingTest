efcfc743197ff3e94df10b40befbdb86e4a4c2f8
BoyCho <53038672+BoyCho@users.noreply.github.com>
2020-11-11 12:04:30 +0900

    Add files via upload

--- ct5.cpp
+++ ct5.cpp
@@ -4,38 +4,30 @@
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    int answer = 0;
-    int lost_n = lost.size();
+    int answer = n;
+    vector<int> stu(31,0);
 
-    vector<int> res, los;
-    res.assign(n, 0);
-    los.assign(n, 0);
-
-    for (int i = 0; i < reserve.size(); i++) {
-        if (reserve[i] > 0)
-            res[reserve[i] - 1]++;
-    }
-    for (int i = 0; i < lost.size(); i++) {
-        if (lost[i] > 0) {
-            los[lost[i] - 1]++;
-            if (res[reserve[i] - 1] > 0) {
-                los[lost[i] - 1]--;
-                res[reserve[i] - 1]--;
-                lost_n--;
-            }
+    for (int itr : reserve) stu[itr]++;
+    for (int itr : lost) stu[itr]--;
+    for (int i = 1; i <= n; i++) {
+        if (stu[i] == -1) {
+            if (stu[i - 1] == 1)
+                stu[i] = stu[i - 1] = 0;
+            else if (stu[i + 1] == 1)
+                stu[i + 1] = stu[i] = 0;
         }
     }
-
-    for (int i = 0; i < lost.size(); i++) {
-        if (res[lost[i] - 2] > 0) {
-            lost_n--;
-            res[lost[i] - 2]--;
-        }
-        else if (res[lost[i]] > 0) {
-            lost_n--;
-            res[lost[i]]--;
-        }
+    
+    for (int i = 1; i <= n; i++) {
+        if (stu[i] == -1) answer--;
     }
-    answer = n - lost_n;
+
     return answer;
 }
+/*
+    조건의 우선 순위를 잘 파악하여 우선 순위대로 나누자
+
+    되도록 추가 배열은 적게 사용해보자
+
+    for(itr : )를 배열의 값을 인덱스로 사용할 떄 사용하자
+*/
