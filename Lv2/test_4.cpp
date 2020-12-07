c581d5313957082297b27aa04686faadd6d18b94
BoyCho <iijokv@naver.com>
2020-12-08 01:34:34 +0900

    124 나라의 숫자

--- Lv2/test_4.cpp
+++ Lv2/test_4.cpp
@@ -4,32 +4,18 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    if (n > 3) {
-        n--;
-        while (n >= 0) {
-            switch (n % 3) {
-            case 0:
-                answer = '1' + answer;
-                break;
-            case 1:
-                answer = '2' + answer;
-                break;
-            case 2:
-                answer = '4' + answer;
-                break;
-            }
-            n /= 3;
-            n--;
-        }
+    while (n) {
+        answer = "412"[n % 3] + answer;
+        if (!n % 3) n--;
+        n /= 3;
     }
-    else
-        switch (n) {
-        case 1:
-            return "1";
-        case 2:
-            return "2";
-        case 3:
-            return "4";
-        }
     return answer;
 }
+
+/*
+    잘못한 점
+    : 1,2,4 를 굳이 순서대로 해석하려 하였다
+      4,1,2 로 해석하였다면 더 쉬웠을 것을 ..
+    배운 점
+    : "string"[n]
+*/
