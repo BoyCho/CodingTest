3d72dc6565de3c19de1acc15c1a4eafd4757e12b
BoyCho <iijokv@naver.com>
2020-12-31 16:11:52 +0900

    카카오 프렌즈 컬러링북

--- Lv2/test_9.cpp
+++ Lv2/test_9.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-int area_check(vector<vector<int>> picture, vector<vector<int>> check, int i, int j, int area_n, int m, int n) {
+int area_check(vector<vector<int>> picture, vector<vector<int>> &check, int i, int j, int area_n, int m, int n) {
     check[i][j] = -1;
     if (m > i + 1) {
-        if (picture[i + 1][j] > 0 && check[i + 1][j] >= 0)
+        if (picture[i + 1][j] > 0 && check[i + 1][j] == 0 && (picture[i][j] == picture[i + 1][j]))
             area_n = area_check(picture, check, i + 1, j, ++area_n, m, n);
     }
     if (i > 0) {
-        if (picture[i - 1][j] > 0 && check[i - 1][j] >= 0)
+        if (picture[i - 1][j] > 0 && check[i - 1][j] == 0 && (picture[i][j] == picture[i - 1][j]))
             area_n = area_check(picture, check, i - 1, j, ++area_n, m, n);
     }
     if (n > j + 1) {
-        if (picture[i][j + 1] > 0 && check[i][j + 1] >= 0)
+        if (picture[i][j + 1] > 0 && check[i][j + 1] == 0 && (picture[i][j] == picture[i][j + 1]))
             area_n = area_check(picture, check, i, j + 1, ++area_n, m, n);
     }
     if (j > 0) {
-        if (picture[i][j - 1] > 0 && check[i][j - 1] >= 0)
+        if (picture[i][j - 1] > 0 && check[i][j - 1] == 0 && (picture[i][j] == picture[i][j - 1]))
             area_n = area_check(picture, check, i, j - 1, ++area_n, m, n);
     }
     return area_n;
@@ -32,11 +32,12 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            if (picture[i][j] > 0 && check[i][j] >= 0) {
+            if (picture[i][j] > 0 && check[i][j] == 0) {
                 number_of_area++;
-                tmp = area_check(picture, check, i, j, 0, m, n);
+                tmp = area_check(picture, check, i, j, 1, m, n);
                 if (max_size_of_one_area < tmp)
                     max_size_of_one_area = tmp;
+                cout << check[i][j] << endl;
             }
         }
     }
