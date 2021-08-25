6509b137a07e9937e3d56c4310e5a22aecbc5b8b
BoyCho <iijokv@naver.com>
2021-08-25 14:03:15 +0900

    더 맵게

--- Lv2/test_14.cpp
+++ Lv2/test_14.cpp
@@ -1,36 +1,21 @@
 #include <string>
 #include <vector>
-#include <math.h>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
-    int answer = 0, leaf;
-    while (true) {
+    priority_queue<int, vector<int>, greater<int>> scov(scoville.begin(), scoville.end());
+    int answer = 0, tmp;
 
-        if (scoville.size() == 1) return scoville[0] >= K ? answer : -1;
+    while (scov.top() < K) {
+        if (scov.size() <= 1) return -1;
 
-        for (int i = 1; true; i++) {
-            if (scoville.size() < pow(2, i)) {
-                leaf = i > 2 ? pow(2, i - 2) - 1 : 1;
-                break;
-            }
-        }
-        for (int i = leaf; i < scoville.size(); i++) {
-            int child = i;
-            do {
-                int root = (child - 1) / 2;
-                if (scoville[child] < scoville[root])
-                    swap(scoville[child], scoville[root]);
-                child = root;
-            } while (child != 0);
-        }
-        if (scoville[0] >= K) return answer;
+        tmp = scov.top();
+        scov.pop();
+        scov.push(tmp + scov.top() * 2);
+        scov.pop();
         answer++;
-
-        scoville[0] += scoville[1] < scoville[2] ? scoville[1] * 2 : scoville[2] * 2;
-        scoville.push_back(scoville[0]);
-        scoville.erase(scoville.begin(), scoville.begin() + 2);
     }
+    return answer;
 }
